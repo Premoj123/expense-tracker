4497df31a2c1a815fbb64008768c99bd1254a40f
premoj <premojmohan30@gmail.com>
2025-12-21 16:53:55 +0530

    Added Load and Save Functions for Loading and Saving JSON files

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ import json
 import datetime 
 #import os
 
+#Loading From JSON
 def load_expenses():
-    pass
+    try:  
+        with open("expenses.json" , "r") as expense_file:
+                return json.load(expense_file)
+    except FileNotFoundError:
+          return []
+    
+
+#Saving Into JSON
+def save_expenses(expenses):
+    with open("expenses.json" , "w") as expense_file:
+         json.dump(expenses,expense_file,indent = 4)
+
 
