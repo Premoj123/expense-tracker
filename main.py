9e224a9a77d0be8850cbd10ba41bf9de1e816616
premoj <premojmohan30@gmail.com>
2025-12-22 11:46:00 +0530

    Minor Fix

--- main.py
+++ main.py
@@ -29,7 +29,65 @@ def show_menu():
     print("6. Exit")
 
 
-#ADD Expense 
-def add_expense():
-     pass
 
+#Handling Choice
+def handle_choice(choice,expenses):
+    if choice == "1":
+        add_expense(expenses)   
+
+    elif choice == "2":
+        view_expenses(expenses)
+
+    elif choice == "3":
+        update_expense(expenses)
+
+    elif choice == "4":
+        delete_expense(expenses)
+
+    elif choice == "5":
+        view_summary(expenses)
+
+    elif choice == "6":
+        save_expenses(expenses)
+        print("Exiting Program....")
+        return
+    
+
+    else:
+        print("Invalid Choice.")
+
+
+#ADD THE EXPENSES
+def add_expense(expenses):
+    pass
+
+
+#VIEw THE EXPENSES
+def view_expenses(expenses):
+    pass
+
+
+#UPDATE THE EXPENSES
+def update_expense(expenses):
+    pass
+
+
+#DELETE THE EXPENSES
+def delete_expense(expenses):
+    pass
+
+
+#VIEW SUMMARY 
+def view_summary(expenses):
+    pass
+
+
+def main():
+    expenses = load_expenses()
+    while True:
+        show_menu()
+        choice = input("Enter your choice: ")
+        handle_choice(choice, expenses)
+
+if __name__ == "__main__":
+    main()
