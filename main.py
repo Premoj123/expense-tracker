71f50438e23638ba244793c5ca9de3ad9d70f01c
premoj <premojmohan30@gmail.com>
2025-12-22 11:39:48 +0530

    Updated Handling Choice Logic as well as implemented main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import datetime 
 #import os
 
+
 #Loading From JSON
 def load_expenses():
     try:  
@@ -16,4 +17,19 @@ def save_expenses(expenses):
     with open("expenses.json" , "w") as expense_file:
          json.dump(expenses,expense_file,indent = 4)
 
+
+#MENU
+def show_menu():
+    print("\n====== EXPENSE TRACKER ======")
+    print("1. Add Expense")
+    print("2. View Expenses")
+    print("3. Update Expense")
+    print("4. Delete Expense")
+    print("5. View Summary")
+    print("6. Exit")
+
+
+#ADD Expense 
+def add_expense():
+     pass
 
